826a930ddad3f15d6eb2a2fb75888d8200674c42
Aaron LaBeau <alabeau@gmail.com>
2024-05-31 13:01:55 -0500

    adding fix for angular service locator

--- cblite/src/engine-locator.ts
+++ cblite/src/engine-locator.ts
@@ -1,8 +1,18 @@
 import { ICoreEngine } from '../coretypes';
 
 export class EngineLocator {
+	private static instance: EngineLocator;
+
 	static readonly key: string = 'default';
 	private static engines: Map<string, ICoreEngine> = new Map();
+	private constructor() { }
+
+	static getInstance(): EngineLocator {
+		if (!EngineLocator.instance) {
+			EngineLocator.instance = new EngineLocator();
+		}
+		return EngineLocator.instance;
+	}
 
 	static registerEngine(key: string, service: ICoreEngine): void {
 		EngineLocator.engines.set(key, service);
